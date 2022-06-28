e6481be179cda601327356bfba497424becc5c76
1728 <1728@DESKTOP-KL0VHA8>
2022-06-28 17:51:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿                                        // Установите IDE Visual Studio. 
+                                        //Создайте новое консольное приложение.
+Console.WriteLine("Hello World!!!");    //Добавьте оператор вывода на экран Console.WriteLine и с его помощью выведите надпись: Hello World!!!
+Console.ReadKey();                      //Добавьте задержку Console.ReadKey, чтобы приложение не закрывалось по завершении выполнения.
